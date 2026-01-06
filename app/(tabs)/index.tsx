b81f7614b82ddac6004d2df52782aaa80bee17a9
Abdullah Madkour <abdallashaban584@gmail.com>
2026-01-06 14:54:47 +0200

    created the emoji picker modal and implemented the logic to select an emoji and display it over the image.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,10 @@
+import EmojiList from "@/components/EmojiList";
+import EmojiPicker from "@/components/EmojiPicker";
+import EmojiSticker from "@/components/EmojiSticker";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/ui/Button";
+import CircleButton from "@/components/ui/CircleButton";
+import IconButton from "@/components/ui/IconButton";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
 import { ImageSourcePropType, StyleSheet, View } from "react-native";
@@ -9,6 +14,11 @@ const PlaceHolderImage = require("@/assets/images/background-image.png");
 export default function HomeScreen() {
   const [selectedImage, setSelectedImage] =
     useState<ImageSourcePropType>(PlaceHolderImage);
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<
+    ImageSourcePropType | undefined
+  >(undefined);
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ["images"],
@@ -18,6 +28,7 @@ export default function HomeScreen() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri as ImageSourcePropType);
+      setShowAppOptions(true);
     } else {
       alert("You didn't select any image");
     }
@@ -26,15 +37,51 @@ export default function HomeScreen() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={selectedImage} />
+        {pickedEmoji && (
+          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+        )}
       </View>
-      <View style={styles.footerContainer}>
-        <Button
-          label="Choose a photo"
-          theme="primary"
-          onPress={pickImageAsync}
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton
+              icon="refresh"
+              label="Reset"
+              onPress={() => {
+                setSelectedImage(PlaceHolderImage);
+                setShowAppOptions(false);
+                setPickedEmoji(undefined);
+              }}
+            />
+            <CircleButton
+              onPress={() => {
+                setShowModal(true);
+              }}
+            />
+            <IconButton icon="save-alt" label="Save" onPress={() => {}} />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            label="Choose a photo"
+            theme="primary"
+            onPress={pickImageAsync}
+          />
+          <Button label="Use this photo" />
+        </View>
+      )}
+      <EmojiPicker
+        isVisible={showModal}
+        onClose={() => {
+          setShowModal(false);
+        }}
+      >
+        <EmojiList
+          onCloseModal={() => setShowModal(false)}
+          onSelect={setPickedEmoji}
         />
-        <Button label="Use this photo" />
-      </View>
+      </EmojiPicker>
     </View>
   );
 }
@@ -53,5 +100,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: "center",
+  },
+  optionsContainer: {
+    position: "absolute",
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: "center",
+    flexDirection: "row",
   },
 });
---
+++ components/EmojiSticker.tsx
@@ -0,0 +1,20 @@
+import { Image } from "expo-image";
+import { ImageSourcePropType, View } from "react-native";
+
+type Props = {
+  imageSize: number;
+  stickerSource: ImageSourcePropType;
+};
+
+const EmojiSticker = ({ imageSize, stickerSource }: Props) => {
+  return (
+    <View style={{ top: -350 }}>
+      <Image
+        source={stickerSource}
+        style={{ width: imageSize, height: imageSize }}
+      />
+    </View>
+  );
+};
+
+export default EmojiSticker;
